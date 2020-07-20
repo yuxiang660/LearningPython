d7239171aff65733237990eed7ccaef553f48977
Ben <yuxiang660@163.com>
2020-07-20 14:09:12 +0800

    add timeout

--- code/process/multiprocess.py
+++ code/process/multiprocess.py
@@ -1,9 +1,14 @@
 import multiprocessing
+import time
 
 def worker(num):
     """worker function"""
     print('Worker', num)
     return
+
+def timeout_worker():
+    time.sleep(2)
+    print('Timout worker')
 
 if __name__ == '__main__':
     jobs = []
@@ -15,3 +20,8 @@ if __name__ == '__main__':
     for job in jobs:
         job.join()
     print('ending')
+
+    p2 = multiprocessing.Process(target=timeout_worker)
+    p2.start()
+    p2.join(1)
+    print('p.is_alive()', p2.is_alive())
